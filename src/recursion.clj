5b1a28781a3819a13584514cc6370d25f61190ce
myer0154 <myer0154@umn.edu>
2016-06-08 23:21:44 -0500

    Update to exercise 15

--- src/recursion.clj
+++ src/recursion.clj
@@ -95,7 +95,13 @@
     (* n (power n (dec k)))))
 
 (defn fib [n]
-  :-)
+  (cond
+    (== n 0)
+      0
+    (== n 1)
+      1
+    :else
+    (+ (fib (- n 1)) (fib (- n 2)))))
 
 (defn my-repeat [how-many-times what-to-repeat]
   [:-])
