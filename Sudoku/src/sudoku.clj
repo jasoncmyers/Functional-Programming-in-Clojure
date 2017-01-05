83d793f7be7f5f772f2342c2dd4ccc9d7a022aef
Jason Myers <myer0154@umn.edu>
2017-01-05 16:30:50 -0600

    Finish Sudoku up to exercise 5

--- Sudoku/src/sudoku.clj
+++ Sudoku/src/sudoku.clj
@@ -3,20 +3,48 @@
 
 (def board identity)
 
+(def all-values #{1, 2, 3, 4, 5, 6, 7, 8, 9})
+
+(def sudoku-board
+  (board [[5 3 0 0 7 0 0 0 0]
+          [6 0 0 1 9 5 0 0 0]
+          [0 9 8 0 0 0 0 6 0]
+          [8 0 0 0 6 0 0 0 3]
+          [4 0 0 8 0 3 0 0 1]
+          [7 0 0 0 2 0 0 0 6]
+          [0 6 0 0 0 0 2 8 0]
+          [0 0 0 4 1 9 0 0 5]
+          [0 0 0 0 8 0 0 7 9]]))
+
+(def solved-board
+  (board [[5 3 4 6 7 8 9 1 2]
+          [6 7 2 1 9 5 3 4 8]
+          [1 9 8 3 4 2 5 6 7]
+          [8 5 9 7 6 1 4 2 3]
+          [4 2 6 8 5 3 7 9 1]
+          [7 1 3 9 2 4 8 5 6]
+          [9 6 1 5 3 7 2 8 4]
+          [2 8 7 4 1 9 6 3 5]
+          [3 4 5 2 8 6 1 7 9]]))
+
 (defn value-at [board coord]
-  nil)
+  (get-in board coord))
 
 (defn has-value? [board coord]
-  nil)
+  (not (= 0 (value-at board coord))))
 
 (defn row-values [board coord]
-  nil)
+  (let [[row-num _] coord]
+    (set (get board row-num))))
 
 (defn col-values [board coord]
-  nil)
+  (let [[_ col-num] coord]
+    (set (map #(get % col-num) board))))
 
 (defn coord-pairs [coords]
-  nil)
+  (vec (for [a coords
+        b coords]
+    [a b])))
 
 (defn block-values [board coord]
   nil)
